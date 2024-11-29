3f6e77904ba0f345d1de6a87f374d9a8bd8c6f09
EsteNoEsMonti <estenoesmonti@gmail.com>
2024-11-29 12:31:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// @ts-nocheck
+document.addEventListener('DOMContentLoaded', () => {
+  const openWatchlist = document.getElementById('openWatchlist');
+  const watchlistModal = document.getElementById('watchlistModal');
+  const closeModal = document.getElementById('closeModal');
+
+  // Mostrar modal
+  openWatchlist.addEventListener('click', () => {
+    watchlistModal.classList.remove('hidden');
+  });
+
+  // Cerrar modal al hacer clic en la "X"
+  closeModal.addEventListener('click', () => {
+    watchlistModal.classList.add('hidden');
+  });
+
+  // Cerrar modal al hacer clic fuera del contenido
+  watchlistModal.addEventListener('click', (e) => {
+    if (e.target === watchlistModal) {
+      watchlistModal.classList.add('hidden');
+    }
+  });
+});
